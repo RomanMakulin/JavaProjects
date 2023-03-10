d9ec500a703ea78bbe2a9f20c743ba19b6e6cbc3
Roman <sup.makulin@mail.ru>
2023-03-10 09:46:34 +0300

    проработка 7 урока

---
+++ CoffeIterator/src/main/java/org/example/Coffe.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Coffe extends Beverage{
+
+}
---
+++ CoffeIterator/src/main/java/org/example/Water.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Water extends Ingridient{
+    public Water(String brand, double volume) {
+        super(brand, volume);
+    }
+}
