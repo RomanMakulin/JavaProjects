33dfd71afbd984f190419580370378a35654b2f5
unknown <WayWayz>
2023-03-14 18:43:02 +0300

    comp homework

--- Seminar4/src/main/java/org/example/calk/Calk.java
+++ Seminar4/src/main/java/org/example/calk/Calk.java
@@ -1,22 +1,17 @@
 package org.example.calk;
 
 import java.util.List;
-import java.util.Objects;
 
 public class Calk {
     public Double sum(List<? extends Number> item) {
         double sum = 0;
-        for (var i : item) {
-            sum += i.doubleValue();
-        }
+        for (var i : item) sum += i.doubleValue();
         return sum;
     }
 
     public Double mult(List<? extends Number> item) {
         double mult = 1;
-        for (var i : item) {
-            mult *= i.doubleValue();
-        }
+        for (var i : item) mult *= i.doubleValue();
         return mult;
     }
 
@@ -24,26 +19,17 @@ public class Calk {
         int index = 1;
         double res = item.get(0).doubleValue();
         while (item.size() > index) {
-            res = res / item.get(index).doubleValue();
+            res /= item.get(index).doubleValue();
             index++;
         }
         return res;
     }
 
-//    public <T> String binaryMethod(T obj) {
-//        if (obj instanceof Double) {
-//            int num = ((Double) obj).intValue();
-//            return Integer.toBinaryString(num);
-//        }else return Integer.toBinaryString(Integer.parseInt(obj.toString()));
-//    }
-
     public <T> String binaryMethod(T obj) {
-
         if (obj instanceof String || obj instanceof Double) {
             int num = (int) Double.parseDouble(obj.toString());
             return Integer.toBinaryString(num);
         } else return Integer.toBinaryString(Integer.parseInt(obj.toString()));
-
     }
 
 }
--- Seminar4/src/main/java/org/example/calk/Program.java
+++ Seminar4/src/main/java/org/example/calk/Program.java
@@ -7,13 +7,10 @@ public class Program {
     public static void main(String[] args) {
 
         Calk cal1 = new Calk();
-        System.out.println(cal1.sum(new ArrayList<>(Arrays.asList(2.0, 3, 1))));
-        System.out.println(cal1.mult(new ArrayList<>(Arrays.asList(2.0, 3, 2.5))));
-        System.out.println();
-        System.out.println(cal1.division(new ArrayList<>(Arrays.asList(30, 2, 3, 2))));
-
-
-        System.out.println(cal1.binaryMethod("20"));
+        System.out.println("sum: " + cal1.sum(new ArrayList<>(Arrays.asList(2.0, 3, 1))));
+        System.out.println("mult: " + cal1.mult(new ArrayList<>(Arrays.asList(2.0, 3, 2.5))));
+        System.out.println("div: " + cal1.division(new ArrayList<>(Arrays.asList(30, 2, 3, 2))));
+        System.out.println("binar: " + cal1.binaryMethod("20.5"));
 
     }
 }
