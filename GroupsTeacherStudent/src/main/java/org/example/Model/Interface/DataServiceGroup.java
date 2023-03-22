857f620014c936c2f47eaf31d57d8d7118e3b25b
unknown <WayWayz>
2023-03-22 15:04:25 +0300

    upd

--- GroupsTeacherStudent/src/main/java/org/example/Model/Interface/DataServiceGroup.java
+++ GroupsTeacherStudent/src/main/java/org/example/Model/Interface/DataServiceGroup.java
@@ -1,9 +1,10 @@
 package org.example.Model.Interface;
 
 import org.example.Model.StudGroup.StudyGroup;
+import org.example.Model.User.Teacher;
 import org.example.Model.User.User;
 import java.util.List;
 
 public interface DataServiceGroup<T> {
-    StudyGroup createGroup(User teacher, List<T> studentList);
+    StudyGroup createGroup(Teacher teacher, List<T> studentList);
 }
--- GroupsTeacherStudent/src/main/java/org/example/Model/Interface/StudyGroupService.java
+++ GroupsTeacherStudent/src/main/java/org/example/Model/Interface/StudyGroupService.java
@@ -1,13 +1,14 @@
 package org.example.Model.Interface;
 
 import org.example.Model.StudGroup.StudyGroup;
+import org.example.Model.User.Teacher;
 import org.example.Model.User.User;
 import java.util.List;
 
 public class StudyGroupService implements DataServiceGroup {
 
     @Override
-    public StudyGroup createGroup(User teacher, List studentList) {
+    public StudyGroup createGroup(Teacher teacher, List studentList) {
         StudyGroup group = new StudyGroup(teacher, studentList);
         return group;
     }
