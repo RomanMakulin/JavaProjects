e903eb2a0b190c6a6ef75f42c7fb89948e8d0b48
Roman <sup.makulin@mail.ru>
2023-06-13 11:06:50 +0300

    create test connection db. at work...

---
+++ Traning/src/main/java/org/example/db/Configs.java
@@ -0,0 +1,9 @@
+package org.example.db;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "631048";
+    protected String dbName = "mydb";
+}
---
+++ Traning/src/main/java/org/example/db/Const.java
@@ -0,0 +1,12 @@
+package org.example.db;
+
+public class Const {
+    public static final String USER_TABLE = "users";
+
+    public static final String USERS_ID = "id";
+    public static final String USERS_FIRST_NAME = "first_name";
+    public static final String USERS_LAST_NAME = "last_name";
+    public static final String USERS_AGE = "age";
+    public static final String USERS_ID_GROUP = "id_group";
+    public static final String USERS_ID_PAYMENT = "id_payment";
+}
