3e37961eaefafdaaa828ac4ad8970689deffacf9
unknown <WayWayz>
2023-08-29 21:40:30 +0300

    homework1

---
+++ homework/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.View.ResultVIew;
+
+public class Main {
+    /**
+     * Точка входа в приложение
+     * @param args аргументы входа
+     */
+    public static void main(String[] args) {
+        new ResultVIew().show();
+    }
+}
--- homework/src/main/java/org/example/View/ResultVIew.java
+++ homework/src/main/java/org/example/View/ResultVIew.java
@@ -1,2 +1,12 @@
-package org.example.View;public class ResultVIew {
+package org.example.View;
+
+import org.example.Controller.Actions;
+
+public class ResultVIew {
+    /**
+     * Функция вывода результата действий калькулятора в консоль
+     */
+    public void show(){
+        System.out.println("Your result: " + Actions.sum(2, 4));
+    }
 }
