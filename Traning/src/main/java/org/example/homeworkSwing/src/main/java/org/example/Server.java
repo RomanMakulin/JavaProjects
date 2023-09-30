a016b7c385617ba9b7bfa58e5aefe83bff2ee500
Roman <sup.makulin@mail.ru>
2023-09-30 23:43:00 +0300

    upd homework

--- Traning/src/main/java/org/example/homeworkSwing/src/main/java/org/example/Server.java
+++ Traning/src/main/java/org/example/homeworkSwing/src/main/java/org/example/Server.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -69,6 +71,34 @@ public class Server extends JFrame {
                 for (String s : stringList) {
                     messageArea.append(s + "\n");
                 }
+            }
+        });
+
+        inputMessage.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER && isLogin){
+                    String line = vars.getNAME() + ": " + inputMessage.getText() + "\n";
+                    messageArea.append(line);
+                    inputMessage.setText(null);
+
+                    try (FileWriter fr = new FileWriter("logs.txt", true)) {
+                        fr.write(line);
+                        fr.flush();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
             }
         });
 
