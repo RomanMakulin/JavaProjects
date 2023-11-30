6581919e767fb91c5b2430401e2478a393a7ee39
Roman <sup.makulin@mail.ru>
2023-11-30 23:05:55 +0300

    delete task text

--- Traning/src/main/java/org/example/Junior/sem2/Program.java
+++ Traning/src/main/java/org/example/Junior/sem2/Program.java
@@ -9,11 +9,6 @@ import java.util.*;
 
 public class Program {
     public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException, NoSuchFieldException {
-//        Создайте абстрактный класс "Animal" с полями "name" и "age".
-//        Реализуйте два класса-наследника от "Animal" (например, "Dog" и "Cat") с уникальными полями и методами.
-//        Создайте массив объектов типа "Animal" и с использованием Reflection API выполните следующие действия:
-//        Выведите на экран информацию о каждом объекте.
-//        Вызовите метод "makeSound()" у каждого объекта, если такой метод присутствует.
 
         List<Animal> animals = Arrays.asList(
                 new CreateAnimal().newAnimal(Cat.class, "Milka", 2),
