8750d18e9b4f0d5c1cbe8f2a086b9bd369172573
unknown <sup.makulin@mail.ru>
2023-12-06 22:15:45 +0300

    first version serialised

--- Traning/src/main/java/org/example/Junior/sem3/Program.java
+++ Traning/src/main/java/org/example/Junior/sem3/Program.java
@@ -1,7 +1,28 @@
 package org.example.Junior.sem3;
 
-public class Program {
-    public static void main(String[] args) {
+import java.io.*;
 
+//        Разработайте класс Student с полями String name, int age, transient double GPA (средний балл).
+//        Обеспечьте поддержку сериализации для этого класса. Создайте объект класса Student и
+//        инициализируйте его данными. Сериализуйте этот объект в файл. Десериализуйте объект обратно
+//        в программу из файла. Выведите все поля объекта, включая GPA, и обсудите, почему значение GPA
+//        не было сохранено/восстановлено.
+public class Program {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Student student = new Student("Roman", 25, 99.9); // объект сериализования
+        Student student1; // для десериализования
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream("student.bin")){
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(student);
+            System.out.println("Объект сериализован");
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream("student.bin")){
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            student1 = (Student) objectInputStream.readObject();
+            System.out.println("Объект десериализован");
+        }
     }
 }
---
+++ Traning/src/main/java/org/example/Junior/sem3/Student.java
@@ -0,0 +1,48 @@
+package org.example.Junior.sem3;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    protected String name;
+    protected int age;
+    transient double gpa;
+
+    public Student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gpa=" + gpa +
+                '}';
+    }
+}
