f50f34b66723ea1b419c0e735fbf2c5aa2916e39
unknown <sup.makulin@mail.ru>
2023-12-11 18:21:33 +0300

    complete hibernate example

--- Traning/src/main/java/org/example/Junior/HibernateTran/DB.java
+++ Traning/src/main/java/org/example/Junior/HibernateTran/DB.java
@@ -1,64 +1,72 @@
 package org.example.Junior.HibernateTran;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class DB {
-    public static void connectDB() {
-        Connector connector = new Connector();
+    Connector connector = new Connector();
+
+    public void updateUserAge(int id, int age){
         try (Session session = connector.getSession()) {
+            String hql = "FROM User where id = :id";
+            Query<User> query = session.createQuery(hql, User.class);
+            query.setParameter("id", id);
 
-            //region update user
-//            String hql = "FROM User where id = :id";
-//            Query<User> query = session.createQuery(hql, User.class);
-//            query.setParameter("id", 2);
-//
-//            User user = query.getSingleResult();
-//            user.setAge(21);
-//
-//            session.beginTransaction();
-//            session.update(user);
-//            session.getTransaction().commit();
-            //endregion
+            User user = query.getSingleResult();
+            user.setAge(age);
 
-            //region show all in console
-//            List<User> userList = session.createQuery("FROM User", User.class).getResultList();
-//            userList.forEach(System.out::println);
-            //endregion
-
-            //region delete all
-//            Transaction transaction = session.beginTransaction();
-//            List<User> userList = session.createQuery("FROM User", User.class).getResultList();
-//
-//            userList.forEach(session::delete);
-//            transaction.commit();
-            //endregion
-
-            //region delete one
-//            String hql = "FROM User where id = :id";
-//            Query<User> query = session.createQuery(hql, User.class);
-//            query.setParameter("id", 2);
-//
-//            session.beginTransaction();
-//            session.delete(query.getSingleResult());
-//            session.getTransaction().commit();
-            //endregion
-
-            //region CREATE USER
-//        User user = new User("Ann", 23);
-//
-//        session.beginTransaction();
-//        session.save(user);
-//
-//        user = new User("Milka", 2); session.save(user);
-//        user = new User("Olga", 50); session.save(user);
-//
-//        session.getTransaction().commit();
-//        session.close();
-            //endregion
-
-        } catch (Exception e) {
+            session.beginTransaction();
+            session.update(user);
+            session.getTransaction().commit();
+        }catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    public void showAllUsersInConsole(){
+        try (Session session = connector.getSession()) {
+            List<User> userList = session.createQuery("FROM User", User.class).getResultList();
+            userList.forEach(System.out::println);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteAllUsers(){
+        try (Session session = connector.getSession()) {
+            Transaction transaction = session.beginTransaction();
+            List<User> userList = session.createQuery("FROM User", User.class).getResultList();
+            userList.forEach(session::delete);
+            transaction.commit();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteOneUserByID(int id){
+        try (Session session = connector.getSession()) {
+            String hql = "FROM User where id = :id";
+            Query<User> query = session.createQuery(hql, User.class);
+            query.setParameter("id", id);
+
+            session.beginTransaction();
+            session.delete(query.getSingleResult());
+            session.getTransaction().commit();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createUser(List<User> user){
+        try (Session session = connector.getSession()) {
+            session.beginTransaction();
+            user.forEach(session::save);
+            session.getTransaction().commit();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- Traning/src/main/java/org/example/Junior/HibernateTran/Main.java
+++ Traning/src/main/java/org/example/Junior/HibernateTran/Main.java
@@ -1,7 +1,20 @@
 package org.example.Junior.HibernateTran;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        DB.connectDB();
+        DB dataBase = new DB();
+
+        dataBase.deleteAllUsers();
+        dataBase.createUser(Arrays.asList(
+                new User("Roman", 25),
+                new User("Ann", 21),
+                new User("Olga", 50),
+                new User("Milka", 2)));
+        dataBase.showAllUsersInConsole();
+        dataBase.updateUserAge(9, 55);
+        dataBase.deleteOneUserByID(8);
+        dataBase.showAllUsersInConsole();
     }
 }
