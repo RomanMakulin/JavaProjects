1616611d2a466c46299db01121061df5c68e9eca
unknown <sup.makulin@mail.ru>
2023-12-16 15:23:29 +0300

    create dir for unit-tests work and tran assertJ

--- Traning/src/main/java/org/example/Junior/HibernateTran/Main.java
+++ Traning/src/main/java/org/example/Junior/HibernateTran/Main.java
@@ -1,20 +1,38 @@
 package org.example.Junior.HibernateTran;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        DB dataBase = new DB();
+//        DB dataBase = new DB();
+//
+//        dataBase.deleteAllUsers();
+//        dataBase.createUser(Arrays.asList(
+//                new User("Roman", 25),
+//                new User("Ann", 21),
+//                new User("Olga", 50),
+//                new User("Milka", 2)));
+//        dataBase.showAllUsersInConsole();
+//        dataBase.updateUserAge(9, 55);
+//        dataBase.deleteOneUserByID(8);
+//        dataBase.showAllUsersInConsole();
 
-        dataBase.deleteAllUsers();
-        dataBase.createUser(Arrays.asList(
-                new User("Roman", 25),
-                new User("Ann", 21),
-                new User("Olga", 50),
-                new User("Milka", 2)));
-        dataBase.showAllUsersInConsole();
-        dataBase.updateUserAge(9, 55);
-        dataBase.deleteOneUserByID(8);
-        dataBase.showAllUsersInConsole();
+        char[] test1 = {'T', 'I', 'N', 'K', 'O', 'F', 'F'};
+        char[] test2 = {1, 'I', 'N', 'K', 3, 'F', 'F'};
+        char[] test3 = {'T', 'I', 'N', 'K', 'O', 'F', 'F', 'F'};
+        char[] test4 = {'F', 'I', 'N', 'K', 'O', 'F', 'F'};
+
+        checkLetters(test3);
+    }
+
+    public static void checkLetters(char[] test){
+        String needLetters = "FFOKNIT";
+        int needSize = needLetters.length();
+
+        String finalResult = new StringBuilder(new String(test)).reverse().toString();
+        if (finalResult.equals(needLetters))System.out.println("YES");
+        else System.out.println("NO");
+        if (finalResult.length() == needSize)System.out.println("YES");
+        else System.out.println("NO");
     }
 }
---
+++ Traning/src/main/java/org/example/UnitTests/tran/Program.java
@@ -0,0 +1,27 @@
+package org.example.UnitTests.tran;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class Program {
+    public static void main(String[] args) {
+        assertThat(calk(4, 2, '-')).isEqualTo(1);
+        System.out.printf("Result: %s", calk(4, 2, '-'));
+    }
+
+    public static int calk(int a, int b, char c) {
+        switch (c) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+            default:
+                System.out.println("wrong input");
+                break;
+        }
+        return 0;
+    }
+}
