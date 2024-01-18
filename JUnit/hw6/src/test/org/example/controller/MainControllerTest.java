fe5a90f227faddb8377949321f28e5a7d8c7060e
unknown <sup.makulin@mail.ru>
2024-01-18 21:39:21 +0300

    upd

--- JUnit/hw6/src/test/org/example/controller/MainControllerTest.java
+++ JUnit/hw6/src/test/org/example/controller/MainControllerTest.java
@@ -10,10 +10,15 @@ import static org.mockito.Mockito.*;
 
 class MainControllerTest {
 
+    /**
+     * Проверка отработки класса, который собирает все воедино.
+     * Для этого в main были вынесены arr list (repo) и arr service (только для использования mock)
+     * Используем заглушку для MainController чтобы не класть внутрь зависимости перечисленные выше
+     */
     @Test
     void btnClick() {
         MainController mainControllerMock = mock(MainController.class);
         mainControllerMock.btnClick();
-        verify(mainControllerMock, times(1)).btnClick();
+        verify(mainControllerMock, times(1)).btnClick(); // проверяем, что метод был вызван
     }
 }
